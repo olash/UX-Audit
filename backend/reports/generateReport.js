f6d12296c26225fa64d2aa246d5c8b99068756e8
Sheriff Abdurrahman <damilolash@gmail.com>
2026-02-18 18:49:58 +0100

    Fix reportTemplate missing module error

--- backend/reports/generateReport.js
+++ backend/reports/generateReport.js
@@ -1,5 +1,5 @@
 // 1. Imports
-import { chromium } from 'playwright';
+import { chromium } from 'playwright-core';
 import path from 'path'; // Step 1: Import path correctly
 import { supabase } from "../db/supabase.js";
 import { renderReportHTML } from './reportTemplate.js';
@@ -46,9 +46,48 @@ export async function generateReport(projectId) {
     try {
         // 2. Launch Browser
         browser = await chromium.launch({
-            args: ['--no-sandbox', '--disable-setuid-sandbox'] // Critical for ECS/Docker
+            headless: true,
+            args: [
+                '--autoplay-policy=user-gesture-required',
+                '--disable-background-networking',
+                '--disable-background-timer-throttling',
+                '--disable-backgrounding-occluded-windows',
+                '--disable-breakpad',
+                '--disable-client-side-phishing-detection',
+                '--disable-component-update',
+                '--disable-default-apps',
+                '--disable-dev-shm-usage',
+                '--disable-domain-reliability',
+                '--disable-extensions',
+                '--disable-features=AudioServiceOutOfProcess',
+                '--disable-hang-monitor',
+                '--disable-ipc-flooding-protection',
+                '--disable-notifications',
+                '--disable-offer-store-unmasked-wallet-cards',
+                '--disable-popup-blocking',
+                '--disable-print-preview',
+                '--disable-prompt-on-repost',
+                '--disable-renderer-backgrounding',
+                '--disable-setuid-sandbox',
+                '--disable-speech-api',
+                '--disable-sync',
+                '--hide-scrollbars',
+                '--ignore-gpu-blacklist',
+                '--metrics-recording-only',
+                '--mute-audio',
+                '--no-default-browser-check',
+                '--no-first-run',
+                '--no-pings',
+                '--no-sandbox',
+                '--no-zygote',
+                '--password-store=basic',
+                '--use-gl=swiftshader',
+                '--use-mock-keychain',
+                '--single-process'
+            ]
         });
-        const page = await browser.newPage();
+        const context = await browser.newContext();
+        const page = await context.newPage();
 
         // 2a. Fetch Data for Report
         // Project
